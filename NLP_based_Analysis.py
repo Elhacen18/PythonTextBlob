80714d933ca67bbfebea288874e7056c275c75c3
Elhacen18 <elhacencheikh18@gmail.com>
2024-04-22 00:15:22 -0400

    finished

--- NLP_based_Analysis.py
+++ NLP_based_Analysis.py
@@ -8,15 +8,13 @@ from operator import itemgetter
 import matplotlib.pyplot as plt 
 from wordcloud import WordCloud   
 import imageio
-
-
-
-# Stops =['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+import spacy
+nlp_sm = spacy.load('en_core_web_sm')
 
 #  Part 2a
 nltk.download('stopwords')
 stop_words = list(stopwords.words('english'))
-fileName = ['Taken_Or_Left_novel.txt','Catching_of_the_whale_and_seal.txt','Extramammary_Articale.txt']
+fileNames = ['Taken_Or_Left_novel.txt','Catching_of_the_whale_and_seal.txt','Extramammary_Articale.txt']
 # From assignment2 
 def Get_Words_Freq(list): # using this fucntion to get word frequancy for all three files as one list. 
     return Counter(list)
@@ -28,7 +26,6 @@ def ReadAllFiles():
     MyList.append(blob2)
     blob3 = TextBlob(Path('Extramammary_Articale.txt').read_text())
     MyList.append(blob3)
-    print(len(MyList))
     return MyList
 def RemoveStopWords(BlobList):
     WordList = []
@@ -37,7 +34,6 @@ def RemoveStopWords(BlobList):
         # "if not word.isdigit()"" will exclude any number in the text. 
         WordList = [word.lower() for word in blob.words if word.lower() not in stop_words if not word.isdigit()]
         MyBlobList.append(WordList) 
-    print(len(MyBlobList))
     return MyBlobList
 def GetWordCount(BlobList):
 
@@ -51,7 +47,7 @@ def GetWordCount(BlobList):
         Wordcount = list(blob.word_counts.items())
         Dic_List.append(Wordcount)
 
-    # print(Dic_List) 
+
   
 
     return Dic_List
@@ -63,7 +59,8 @@ def Top25WordsPlot(list):
         file = sorted(item, key=itemgetter(1), reverse=True)
         # add only top 25 words 
         top25Words.append(file[:25])
-    # print(top25Words)
+
+
     fig, axis = plt.subplots(3,1,figsize=(20, 8))
 
 
@@ -73,7 +70,7 @@ def Top25WordsPlot(list):
 
         Words = [key for key in dict(top25Words[i])]
         ax.bar(keys_list, values_list, color='green')
-        ax.set_title(f'{fileName[i]} top {len(top25Words[i])} words')
+        ax.set_title(f'{fileNames[i]} top {len(top25Words[i])} words')
         ax.set_xlabel('words')
         ax.set_ylabel('Count')
         ax.tick_params(labelrotation=50)  # Rotate labels for better readability
@@ -88,7 +85,7 @@ def Displa100yWordCloud(List):
         file = sorted(item, key=itemgetter(1), reverse=True)
     # add only top 100 words 
         Top100words.append(file[:100]) 
-    # print(Top100words)
+
 
     mask_image = imageio.v2.imread('AppleLogo.png') 
     wc = WordCloud(width=1000, height=1000,
@@ -102,7 +99,7 @@ def Displa100yWordCloud(List):
         wc.fit_words(dict(Top100words[i]))
         # i+1 will place each subplot in the correct position inside of the figure.
         ax =fig.add_subplot(1, 3, i+1) # print(i)
-        ax.set_title(f'{fileName[i]} top {len(Top100words[i])} words')
+        ax.set_title(f'{fileNames[i]} top {len(Top100words[i])} words')
         ax.imshow(wc, interpolation="bilinear")
         ax.axis('off')
     plt.tight_layout()   
@@ -110,7 +107,6 @@ def Displa100yWordCloud(List):
 def Top25cumulativeWord(lists):
     AllWordlist =[]
     SortList = []
-    # print(lists)
 
     # "for list in list" Merge sublists into one list
     AllfilesMerged = [list for list in lists for list in list] 
@@ -120,11 +116,10 @@ def Top25cumulativeWord(lists):
     
     SortList =  sorted(AllWordlist, key=itemgetter(1), reverse=True)
     Top25CumalativeWords=  SortList[:25]
-    # print(Top25CumalativeWords)
+
     return Top25CumalativeWords
 
 def OutPutToCSVFile(words):
-    # print(words)
     with open("MCW.csv", 'w') as file:
         sum = 0
         file.write(f"word,Count\n")
@@ -132,21 +127,40 @@ def OutPutToCSVFile(words):
             sum = sum + word[1]
             file.write(f"{word[0]},{word[1]}\n")
         file.write(f"Sum,{sum}")
-def ListsToText(FilesWoStopWord):
-    TextList =[]
-    for text in FilesWoStopWord:
-        Words = [word for word in text ]
-        Text = " ".join(Words)
-        TextList.append(Text)
-    print(TextList)
+def FilesTextatistic(Files):
+    AllFileTextStats = []
+    TextStatsByFile = []
+    avgScores = []
+    for file in Files:
+        Text = Textatistic(file)
+        avgScores.append(Text.fleschkincaid_score)
+        avgScores.append(Text.gunningfog_score)
+        avgScores.append(Text.smog_score)
+        avgScores.append(Text.dalechall_score)
+        TextStatsByFile = (sum(avgScores))/(len(avgScores))
+        AllFileTextStats.append({file:TextStatsByFile})
+  
+    # print(AllFileTextStats)    
+    for file in AllFileTextStats:
+        for Filename, avgScore in file.items():
+            print(f"File name: {Filename}\nFlesch- Kincaid, Gunning Fog, SMOG, and Dale-Chall avg score:\n{avgScore}\n\n")
 
-    return
+
+def pairwise_similarity(FileList):
+
+    files = [nlp_sm(file) for file in FileList]
+    print(files)
+    for i in range(len(files)):
+        for j in range(i + 1, len(files)):
+            similarity_scores = files[i].similarity(files[j])
+            print(f"{files[i]} and  {files[j]} Similarity: {f"{similarity_scores:.5f}"}")
+
 # Part 1
 ThreeFilesBlob = ReadAllFiles()
 FilesWoStopWord= RemoveStopWords(ThreeFilesBlob)
-print(FilesWoStopWord)
 # Par 2a
 WordCountByFiles = GetWordCount(FilesWoStopWord) 
+print(WordCountByFiles)
 # Part 2b
 Top25WordsPlot(WordCountByFiles)
 
@@ -157,8 +171,10 @@ top25cumlativeword =Top25cumulativeWord(FilesWoStopWord)
 # part 3b
 OutPutToCSVFile(top25cumlativeword)
 # part 4a
-ListsToText(FilesWoStopWord)
-# TextAtistic = Textatistic(FilesWoStopWord)
-# print(TextAtistic.flesch_score)
+FilesTextatistic(fileNames)
+
+# Part 5a
+pairwise_similarity(fileNames)
+
 
 
